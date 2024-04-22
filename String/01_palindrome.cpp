d12ec0e55030e6fd32668e7ec206f7191bda7db3
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-04-22 18:32:12 +0000

    dsa

---
+++ String/01_palindrome.cpp
@@ -0,0 +1,43 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;	
+	
+	string reverse(string S)
+	{
+	    int st = 0 ; 
+	    int e = S.length()-1;
+	    while( st < e)
+	    {
+	        swap(S[st], S[e]);
+	        st++;
+	        e--;
+	    }
+	    return S;
+	}
+	int isPalindrome(string S)
+	{
+	string original = S;
+    string rev = reverse(S);
+    return original == rev;
+    
+	}
+int main()
+{
+    string s;
+    cout<<" Enter String here : ";
+    getline(cin,s);
+   if(isPalindrome(s))
+   {
+       cout<<" Given String is Palindrome "<<endl;
+   }
+   else 
+   {
+       cout<<" Given String is not Palindrome "<<endl;
+   }
+    
+   
+
+    return 0;
+}
