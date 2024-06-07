aa39fb4bb93c8ce0d88e1dc3be63d016c0da0259
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-06-07 09:39:49 +0000

    dsa

---
+++ Number/09_add_two_number_without_using_+_operator .cpp
@@ -0,0 +1,14 @@
+
+//using increment , decrement 
+class Solution{   
+public:
+    int addition(int A, int B){
+        while(B!=0)
+        {
+        //step1 : Increment, decrement
+        A++,B--;
+        }
+        return A;
+       
+    }
+};
