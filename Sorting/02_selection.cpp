cd0fa6556b5c67579ef9217d617402dac6ecb50a
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-07-28 19:43:17 +0530

    02_selection.cpp

--- Sorting/02_selection.cpp
+++ Sorting/02_selection.cpp
@@ -1,19 +1,20 @@
- void selectionSort(int arr[], int n)
-    {
-        for(int i=0;i<n-1;i++)
-        {
-              int mini = i ;
-            for(int j = i+ 1 ; j <n  ;j++)
-            { 
-                 if(arr[j]<arr[mini])
-                 mini = j;
-            }
-             
-            swap(arr[i],arr[mini]);
-        }
-          
-    }
-    /*
-    Time complexity : O(n^2)
-    Space complexity : O(1)
-    */
+#include <bits/stdc++.h> 
+void selectionSort(vector<int>& arr, int n)
+{   //we have itrate outer loop till n - 2 because last element is always sorted 
+   for(int i =0 ; i <= n - 2 ; i++)
+   {
+       int mini = i ;
+       for(int j = i ; j <= n -1 ; j++)
+       {
+           if(arr[j]<arr[mini])
+           {
+               mini = j;
+           }
+       }
+       swap(arr[i],arr[mini]);
+   }
+}
+/*
+ TC : O(n^2)
+ SC : O(1)
+ */
