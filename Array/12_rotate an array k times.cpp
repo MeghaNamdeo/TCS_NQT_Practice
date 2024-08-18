c1f2434f4936725c99d6aa2f712ff0e2560fdcb6
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-08-18 20:10:44 +0530

    12_rotate an array k times.cpp

--- Array/12_rotate an array k times.cpp
+++ Array/12_rotate an array k times.cpp
@@ -1,3 +1,21 @@
+//Brute force 
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+         vector<int>temp(nums.size());
+         for(int i=0;i<nums.size();i++)
+         {
+            temp[(i+k)%nums.size()]=nums[i];
+         }
+         nums=temp;
+    }
+};
+/*
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
 class Solution{
     public:
     
