a7203fc5326cbee2643473f8f2180419a6f846f8
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-08-18 20:10:04 +0530

    11_Find all repeating elements in an array.cpp

--- Array/11_Find all repeating elements in an array.cpp
+++ Array/11_Find all repeating elements in an array.cpp
@@ -28,3 +28,33 @@ class Solution{
     }
     
 };
+
+class Solution{
+  public:
+    vector<int> duplicates(long long arr[], int n) {
+       vector<int> ans;
+        unordered_map<long long, int> freq;
+
+        for (int i = 0; i < n; i++) {
+            freq[arr[i]]++;
+        }
+
+        for (auto i : freq) {
+            if (i.second !=1 ) {
+                ans.push_back(i.first);
+            }
+        }
+
+        if (ans.empty()) {
+            ans.push_back(-1);
+        }
+        else
+        {
+            sort(ans.begin(),ans.end());
+        }
+
+        return ans;
+    }
+};
+
+
